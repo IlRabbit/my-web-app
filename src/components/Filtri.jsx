1c0f0d32ea0b20f3deca0729ba259ce13fb2ca85
Daniele Terracciano <daniele.terracciano@business-changers.it>
2023-10-01 17:47:33 +0200

    filters refactor

--- src/components/Filtri.jsx
+++ src/components/Filtri.jsx
@@ -1,66 +1,111 @@
-import React, {useState} from 'react'
-import {Form, Button} from 'react-bootstrap/'
-import { searchSpell } from '../services/service'
+import React, {useEffect, useState} from 'react'
+import {Form, Button, Pagination, Row, Col} from 'react-bootstrap/'
+import Select from 'react-select';
 
-const Filtri = ({setSpells}) => {
-  const [value, setValue] = useState('')
 
-    const search = async () => {
-        try {
-            await searchSpell(value).then(res => setSpells(res.results))
-        }
-        catch (err) {console.error(err)}
-    }
+const Filtri = ({query, setQuery}) => {
+  const [search, setSearch] = useState('');
+  const [level, setLevel] = useState(null);
+  const [school, setSchool] = useState(null);
+  const [classe, setClasse] = useState(null);
+  const [searchClicked, setSearchClicked] = useState(false);
+
+  useEffect(() => {
+    if (searchClicked) 
+      setQuery(
+        `?page=${'1'}${search.length > 0 ? '&search=' + search : ''}${level ? '&level_int=' + level : ''}${school ? '&school=' + school : ''}${classe ? '&spell_lists=' + classe : ''}`
+      );
+    setSearchClicked(false);
+    setSearch('');
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [searchClicked])
+
+  useEffect(() => {
+    setQuery(
+      `?page=${'1'}${search.length > 0 ? '&search=' + search : ''}${level ? '&level_int=' + level : ''}${school ? '&school=' + school : ''}${classe ? '&spell_lists=' + classe : ''}`
+    );
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [level, school, classe])
 
   return (
     <>
     <div className='h4 text-white'>Filters</div>
-    <div>
-      <Form className='filtri d-flex'>
-        <Form.Select 
-          aria-label='Default Filter'
-          value={value}
-          onChange={(e) => {
-            console.log(e.target.value); 
-            setValue(e.target.value)
-          }}
-        >
-          <option>Select school...</option>
-          <option>abjuration</option>
-          <option>conjuration</option>
-          <option>divination</option>
-          <option>enchantment</option>
-          <option>evocation</option>
-          <option>illusion</option>
-          <option>necromancy</option>
-          <option>transmutation</option>
-        </Form.Select>
-        <Form.Select aria-label="Default select example">
-          <option>Select level...</option>
-          <option>1st-level</option>
-          <option>2nd-level</option>
-          <option>3rd-level</option>
-          <option>4th-level</option>
-          <option>5th-level</option>
-          <option>6th-level</option>
-          <option>7th-level</option>
-          <option>8th-level</option>
-          <option>9th-level</option>
-        </Form.Select>
-        <Form.Select aria-label="Default select example">
-          <option>Select Class...</option>
-          <option>Bard</option>
-          <option>Cleric</option>
-          <option>Druid</option>
-          <option>Paladin</option>
-          <option>Ranger</option>
-          <option>Sorcerer</option>
-          <option>Warlock</option>
-          <option>Wizard</option>
-        </Form.Select>
-        <Button onClick={search}>Filter</Button>
-      </Form>
-    </div>
+    <Form className="d-flex">
+    <Form.Control
+      type="text"
+      placeholder="Fireball..."
+      className="me-2"
+      aria-label="Search"
+      value={search}
+      onChange={(e) => {console.log(e.target.value); setSearch(e.target.value)}}
+    />
+    <Button 
+      variant="outline-primary"
+      onClick={() => {setSearchClicked(true)}}
+    >
+      Search
+    </Button>
+  </Form>
+    <Row className='mt-5'>
+      <Col>
+        <Select
+          value={school}
+          placeholder={school ?? 'All'}
+          onChange={(opt) => {setSchool(opt.value)}}
+          options={[
+            {value: null, label: 'All'},
+            {value: 'abjuration', label: 'Abjuration'},
+            {value: 'conjuration', label: 'Conjuration'},
+            {value: 'divination', label: 'Divination'},
+            {value: 'enchantment', label: 'Enchantment'},
+            {value: 'evocation', label: 'Evocation'},
+            {value: 'illusion', label: 'Illusion'},
+            {value: 'necromancy', label: 'Necromancy'},
+            {value: 'transmutation', label: 'Transmutation'}
+          ]}
+        />
+      </Col>
+
+      <Col>
+        <Select
+          value={level}
+          placeholder={level ?? 'All'}
+          onChange={(opt) => {setLevel(opt.value)}}
+          options={[
+            {value: null, label: 'All'},
+            {value: '0', label: 'Cantrip'},
+            {value: '1', label: '1st'},
+            {value: '2', label: '2nd'},
+            {value: '3', label: '3rd'},
+            {value: '4', label: '4th'},
+            {value: '5', label: '5th'},
+            {value: '6', label: '6th'},
+            {value: '7', label: '7th'},
+            {value: '8', label: '8th'},
+            {value: '9', label: '9th'}
+          ]}
+        />
+      </Col>
+
+      <Col>
+        <Select
+          value={classe}
+          placeholder={classe ?? 'All'}
+          onChange={(opt) => {setClasse(opt.value)}}
+          options={[
+            {value: null, label: 'All'},
+            {value: 'artificier', label: 'Artificier'},
+            {value: 'bard', label: 'Bard'},
+            {value: 'druid', label: 'Druid'},
+            {value: 'paladin', label: 'Paladin'},
+            {value: 'ranger', label: 'Ranger'},
+            {value: 'sorcerer', label: 'Sorcerer'},
+            {value: 'warlock', label: 'Warlock'},
+            {value: 'wizard', label: 'Wizard'},
+          ]}
+        />
+      </Col>
+    </Row>
     </>
   )
 }
--- src/services/service.jsx
+++ src/services/service.jsx
@@ -3,5 +3,3 @@ const baseUrl = 'https://api.open5e.com/v1';
 export const getSpells = (query = null) => 
     fetch(`${baseUrl}/spells/${query ?? ''}`).then(res => res.json());
 
-export const searchSpell = (name) => 
-    fetch(`${baseUrl}/spells/?search=${name}`).then(res => res.json());
